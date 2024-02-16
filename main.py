54024e746c56eb38d247254355bd65491f389b73
AdrianGerman <adrian.german1019@gmail.com>
2024-02-16 08:54:39 -0600

    poke list

--- main.py
+++ main.py
@@ -1,22 +1,12 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+from pokemon_list import pokemon
 
 app = FastAPI()
 
 app.title = 'PokeAPI from FastAPI'
 # app.version = '0.0.1'
 
-pokemon = [
-    {
-    'id': 1,
-    'title': 'Bulbasaur',
-    'type': 'Planta, Veneno',
-    'description': 'Lleva un bulbo en el lomo desde que nace. A medida que el Pokémon crece, el bulbo también va haciéndose más grande.',
-    'category': 'Semilla',
-    'height': 0.7,
-    'weight': 6.9,
-    }
-]
 
 @app.get('/', tags=['home'])
 def message():
---
+++ pokemon_list.py
@@ -0,0 +1,29 @@
+pokemon = [
+    {
+    'id': 1,
+    'title': 'Bulbasaur',
+    'type': 'Planta, Veneno',
+    'description': 'Lleva un bulbo en el lomo desde que nace. A medida que el Pokémon crece, el bulbo también va haciéndose más grande.',
+    'category': 'Semilla',
+    'height': 0.7,
+    'weight': 6.9,
+    },
+    {
+    'id': 2,
+    'title': 'Ivysaur',
+    'type': 'Planta, Veneno',
+    'description': 'El capullo de la espalda crece al absorber nutrientes. Desprende un aroma delicioso cuando está a punto de florecer.',
+    'category': 'Semilla',
+    'height': 1.0,
+    'weight': 13.0,
+    },
+    {
+    'id': 3,
+    'title': 'Venusaur',
+    'type': 'Planta, Veneno',
+    'description': 'Llena su cuerpo de energía con los rayos solares que capta al extender los enormes pétalos de su flor.',
+    'category': 'Semilla',
+    'height': 2.0,
+    'weight': 100.0,
+    }
+]
