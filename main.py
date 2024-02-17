a0565e330f52a8ab6de47b2ce099a5a1250c5a31
AdrianGerman <adrian.german1019@gmail.com>
2024-02-17 08:34:17 -0600

    put and delete

--- main.py
+++ main.py
@@ -43,3 +43,25 @@ def create_pokemon(id: int = Body(), name: str = Body(), type: list = Body(...),
     })
     return pokemons
 
+@app.put('/pokemons/{id}', tags=['pokemon'])
+def update_pokemon(id: int, name: str = Body(), type: list = Body(...), description: str = Body(), category: str = Body(), height: float = Body(), weight: float = Body()):
+    for item in pokemons:
+        if item['id'] == id:
+            item['name'] = name,
+            item['type'] = type,
+            item['description'] = description,
+            item['category'] = category,
+            item['height'] = height,
+            item['weight'] = weight,
+            return pokemons
+            
+            
+            
+@app.delete('/pokemons/{id}', tags=['pokemon'])
+def delete_pokemon(id: int):
+    for item in pokemons:
+        if item['id'] == id:
+            pokemons.remove(item)
+            return pokemons
+            
+
