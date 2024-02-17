605127165983250356d0a218cc16147d0b9de5a0
AdrianGerman <adrian.german1019@gmail.com>
2024-02-17 14:10:09 -0600

    data type validations with pydantic

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Body
 from fastapi.responses import HTMLResponse
 from pokemon_list import pokemons
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional
 
 from typing import List, Optional
@@ -9,25 +9,47 @@ from typing import List, Optional
 app = FastAPI()
 
 app.title = 'PokeAPI from FastAPI'
-# app.version = '0.0.1'
+app.version = '0.0.1'
+
 
 class Pokemon(BaseModel):
     id: Optional[int] = None
-    name: str
-    type: list
-    description: str
-    category: str
-    height: float
-    weight: float
+    name: str = Field(default="Pokémon", max_length=15)
+    type: list = Field(default=["Normal", "Fuego", "Agua"])
+    description: str = Field(default="Un pokémon salvaje")
+    category: str = Field(default="Animal")
+    height: float = Field(default=1.2)
+    weight: float = Field(default=0.35)
+
+    # este tipo de modelo dejo de funcionar hace años
+    # lo dejo aquí por si algún día vuelve a funcionar
+
+    # model_config = {
+    #     "json_schema_extra": {
+    #         "examples": [
+    #             {
+    #                 "id": 7,
+    #                 "name": "Pokémon",
+    #                 "type": ["Normal", "Fuego", "Agua"],
+    #                 "description": "Un pokémon salvaje",
+    #                 "category": "Animal",
+    #                 "height": 1.2,
+    #                 "weight": 0.35,
+    #             }
+    #         ]
+    #     }
+    # }
 
 
 @app.get('/', tags=['home'])
 def message():
     return HTMLResponse('<h1>Hello Germán</h1>')
 
+
 @app.get('/pokemons', tags=['pokemon'])
 def get_pokemon():
     return pokemons
+
 
 @app.get('/pokemons/{id}', tags=['pokemon'])
 def get_pokemon_id(id: int):
@@ -36,8 +58,9 @@ def get_pokemon_id(id: int):
             return item
     return []
 
+
 @app.get('/pokemons/by_type/{type}', tags=['pokemon'])
-def get_pokemon_by_type(type: str):    
+def get_pokemon_by_type(type: str):
     return list(filter(lambda pokemon: type in pokemon['type'], pokemons))
 
 
@@ -45,6 +68,7 @@ def get_pokemon_by_type(type: str):
 def create_pokemon(pokemon: Pokemon):
     pokemons.append(pokemon)
     return pokemons
+
 
 @app.put('/pokemons/{id}', tags=['pokemon'])
 def update_pokemon(id: int, pokemon: Pokemon):
@@ -57,14 +81,11 @@ def update_pokemon(id: int, pokemon: Pokemon):
             item['height'] = pokemon.height
             item['weight'] = pokemon.weight
             return pokemons
-            
-            
-            
+
+
 @app.delete('/pokemons/{id}', tags=['pokemon'])
 def delete_pokemon(id: int):
     for item in pokemons:
         if item['id'] == id:
             pokemons.remove(item)
             return pokemons
-            
-
