a38887a368bc0326ca8d032025520b56c79992f3
AdrianGerman <adrian.german1019@gmail.com>
2024-02-18 10:06:57 -0600

    dotenv

--- jwt_manager.py
+++ jwt_manager.py
@@ -1,6 +1,12 @@
+import os
+from dotenv import load_dotenv
 from jwt import encode
 
+load_dotenv
 
-def create_token(data: dict):
-    token: str = encode(payload=data, key="my_key", algorithm='HS256')
+secretKey = os.getenv("SECRET_KEY")
+
+
+def create_token(data: dict) -> str:
+    token: str = encode(payload=data, key="secretKey", algorithm="HS256")
     return token
