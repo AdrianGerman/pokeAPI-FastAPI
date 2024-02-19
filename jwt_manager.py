5dd91bcbbfa6c4ca0ee0a13ba16246d08d40d894
AdrianGerman <adrian.german1019@gmail.com>
2024-02-19 12:21:46 -0600

    bug fix in jwt_managment

--- jwt_manager.py
+++ jwt_manager.py
@@ -2,16 +2,16 @@ import os
 from dotenv import load_dotenv
 from jwt import encode, decode
 
-load_dotenv
+load_dotenv()
 
-secretKey = os.getenv("SECRET_KEY")
+secretKey = os.getenv('SECRET_KEY')
 
 
 def create_token(data: dict) -> str:
-    token: str = encode(payload=data, key=secretKey, algorithm="HS256")
+    token: str = encode(payload=data, key=secretKey, algorithm='HS256')
     return token
 
 
 def validate_token(token: str) -> dict:
-    data: dict = decode(token, key=secretKey, algorithm=["HS256"])
+    data: dict = decode(token, key=secretKey, algorithms=['HS256'])
     return data
