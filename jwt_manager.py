7e2f052ef632bd27048b77a01ed5797dccea7d27
AdrianGerman <adrian.german1019@gmail.com>
2024-02-19 10:41:30 -0600

    validating token with PyJWT

--- jwt_manager.py
+++ jwt_manager.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from jwt import encode
+from jwt import encode, decode
 
 load_dotenv
 
@@ -10,3 +10,8 @@ secretKey = os.getenv("SECRET_KEY")
 def create_token(data: dict) -> str:
     token: str = encode(payload=data, key="secretKey", algorithm="HS256")
     return token
+
+
+def validate_token(token: str) -> dict:
+    data: dict = decode(token, key="secretKey", algorithm=["HS256"])
+    return data
--- main.py
+++ main.py
@@ -54,7 +54,9 @@ def message():
 
 @app.post('/login', tags=['auth'])
 def login(user: User):
-    return user
+    if user.email == 'admin@gmail.com' and user.password == 'admin':
+        token: str = create_token(user.model_dump())
+        return JSONResponse(status_code=200, content=token)
 
 
 @app.get('/pokemons', tags=['pokemon'], response_model=List[Pokemon])
